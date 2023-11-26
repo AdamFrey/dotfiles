034d1b823186347eae6f94f8ad19454c1bfa38c1
Adam Frey <adam@adamfrey.me>
2023-11-26 14:35:27 -0500

    Turn on some new modes for doom emacs

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -24,11 +24,11 @@
 
        :ui
        ;;deft              ; notational velocity for Emacs
-       doom            ; what makes DOOM look the way it does
-       doom-dashboard  ; a nifty splash screen for Emacs
-       doom-quit       ; DOOM quit-message prompts when you quit Emacs
+       doom                   ; what makes DOOM look the way it does
+       doom-dashboard         ; a nifty splash screen for Emacs
+       doom-quit              ; DOOM quit-message prompts when you quit Emacs
        ;;fill-column       ; a `fill-column' indicator
-       hl-todo     ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       hl-todo                ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
        ;;indent-guides     ; highlighted indent columns
        ;;ligatures         ; ligatures and symbols to make your code pretty again
@@ -42,7 +42,7 @@
        ;;tabs              ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        ;;unicode           ; extended unicode support for various languages
-       (vc-gutter +pretty)              ; vcs diff in the fringe
+       (vc-gutter +pretty)    ; vcs diff in the fringe
        vi-tilde-fringe        ; fringe tildes to mark beyond EOB
        ;;window-select     ; visually switch windows
        workspaces             ; tab emulation, persistence & separate workspaces
@@ -62,7 +62,7 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       (dired +icons)             ; making dired pretty [functional]
+       (dired +icons)    ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
@@ -94,11 +94,12 @@
        magit                    ; a git porcelain for Emacs
        make                     ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
-       ;;pdf               ; pdf enhancements
+       pdf                              ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        terraform  ; infrastructure as code
+       tree-sitter
        ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
 
@@ -125,10 +126,10 @@
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
-       (haskell +lsp)      ; a language that's lazier than I am
+       (haskell +lsp)                   ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
-       json                ; At least it ain't XML
+       json                 ; At least it ain't XML
        (java +meghanada)    ; the poster child for carpal tunnel syndrome
        (javascript)         ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
@@ -161,7 +162,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       (web +lsp +tree-sitter)                              ; the tubes
+       (web +lsp +tree-sitter)          ; the tubes
        yaml                             ; JSON, but readable
        zig
        :email
