3a5fe9e48b68ef183a35805b3edae0cba755f4a0
Adam Frey <adam@adamfrey.me>
2023-11-30 14:04:44 -0500

    Install more packages for development

--- dot_config/distrobox/executable_devtools-bootstrap.sh
+++ dot_config/distrobox/executable_devtools-bootstrap.sh
@@ -16,21 +16,27 @@ local_install openssh
 local_install podman
 local_install rlwrap
 local_install tldr
-local_install terraform
+
 local_install unzip
-yay_i google-cloud-cli
+
 
 export_install chezmoi
 export_install clojure
 export_install clojure clj
 export_install leiningen lein
 export_install npm
+export_install packer
 export_install postgresql psql
 export_install shellcheck
 export_install_yay babashka-bin bb
 export_install_yay circleci-cli-bin circleci
 export_install_yay cljfmt-bin cljfmt
 export_install_yay clj-kondo-bin clj-kondo
+export_install terraform
+
+yay_i google-cloud-cli gcloud
+distrobox-export --bin /opt/google-cloud-cli/bin/gcloud --export-path ~/.local/bin
+distrobox-export --bin /opt/google-cloud-cli/bin/gsutil --export-path ~/.local/bin
 
 
 
