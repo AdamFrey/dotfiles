496ae962aa2f3b1e0b63cbb3c973da4cb7b324df
Adam Frey <adam@adamfrey.me>
2023-12-07 19:37:38 -0500

    Add emacs .custom.el vars

--- dot_doom.d/custom.el
+++ dot_doom.d/custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7964b513f8a2bb14803e717e0ac0123f100fb92160dcf4a467f530868ebaae3e" "c5878086e65614424a84ad5c758b07e9edcf4c513e08a1c5b1533f313d1b17f1" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "ffafb0e9f63935183713b204c11d22225008559fa62133a69848835f4f4a758c" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(safe-local-variable-values
    '((cider-merge-sessions . project)
      (format-all-mode)
