e04833f9ea2c7512a563f02dd361203f1fa69bc2
Adam Frey <adam@adamfrey.me>
2023-12-07 21:50:27 -0500

    Install new packages

--- dot_config/distrobox/executable_desktop-tools-bootstrap.sh
+++ dot_config/distrobox/executable_desktop-tools-bootstrap.sh
@@ -12,9 +12,10 @@ local_install zoxide
 
 export_install restic
 export_install xorg-xset xset
-export_install fasd
+export_install feh
 export_install fzf
 export_install polybar
 export_install tldr
+
 
 yay_i ttf-icomoon-feather-git
