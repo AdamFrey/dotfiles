96038ae6780aff09365cc174e64952709928d69c
Adam Frey <adam@adamfrey.me>
2023-12-11 08:45:26 -0500

    Fix fzf installation

--- dot_config/distrobox/executable_desktop-tools-bootstrap.sh
+++ dot_config/distrobox/executable_desktop-tools-bootstrap.sh
@@ -8,12 +8,12 @@ local_install openssh
 local_install kitty-terminfo
 local_install direnv
 local_install dunst
+local_install fzf
 local_install zoxide
 
 export_install restic
 export_install xorg-xset xset
 export_install feh
-export_install fzf
 export_install polybar
 export_install tldr
 
