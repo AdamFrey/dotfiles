bc0d3bd829a05851108bd8cd8c86bc027e8dcac2
Adam Frey <adam@adamfrey.me>
2024-02-13 08:38:13 -0500

    Add portal inspector to user.clj

--- dot_config/clojure/my.dev-tools/src/user.clj
+++ dot_config/clojure/my.dev-tools/src/user.clj
@@ -4,14 +4,24 @@
  '[snitch.core :refer [defn* defmethod* *fn *let]]
  '[clojure.java.javadoc :refer (javadoc)]
  '[clojure.pprint :refer (pp pprint)]
+ '[portal.api :as portal]
   ;; '[dev.nu.morse :as morse]
  )
 
 (intern 'clojure.core (with-meta 'ppr (meta #'pprint)) #'pprint)
 (intern 'clojure.core (with-meta 'pp (meta #'pp)) #'pp)
 (intern 'clojure.core (with-meta 'javadoc (meta #'javadoc)) #'javadoc)
+;; (intern 'clojure.core (with-meta 'portal (meta #'open)) #'open)
 ;; (intern 'clojure.core (with-meta 'insp (meta #'morse/inspect)) #'morse/inspect)
 ;; (morse/launch-in-proc)
+
+(declare p)
+
+(defn portal!
+  []
+  (prn "starting portal...")
+  (def p (portal/open))
+  (add-tap #'portal/submit))
 
 (let [time*
       (fn [^long duration-in-ms f]
