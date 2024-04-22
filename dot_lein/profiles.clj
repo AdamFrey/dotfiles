fd21edace8ae40e799fa1e22673774bf9776f1ac
Adam Frey <adam@adamfrey.me>
2024-04-22 08:43:23 -0400

    Add cider deps to lein/profiles.clj

--- dot_lein/profiles.clj
+++ dot_lein/profiles.clj
@@ -1,6 +1,8 @@
 {:user {:dependencies
         [[djblue/portal "0.51.1"]
          [org.clojars.abhinav/snitch "0.1.15"]
-         [com.clojure-goes-fast/clj-java-decompiler "0.3.3"]]
+         [com.clojure-goes-fast/clj-java-decompiler "0.3.3"]
+         [cider/cider-nrepl "0.45.0"]
+         [refactor-nrepl/refactor-nrepl "3.9.1"]]
         :resource-paths ["/home/adam/.config/clojure/my.dev-tools/src"]
         :plugins        [[cddr/lein-pprint "1.3.2"]]}}
