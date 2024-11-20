1db6d0d20b2a5f7798b2df752ea47f61e0825888
Adam Frey <adam@adamfrey.me>
2024-11-20 12:11:42 -0500

    Add emacs function to reload dir locals

--- dot_doom.d/config.el
+++ dot_doom.d/config.el
@@ -68,6 +68,28 @@
   :config
   (cascading-dir-locals-mode 1))
 
+(defun af/reload-dir-locals (proj)
+  "Read values from the current project's .dir-locals file and
+apply them in all project file buffers as if opening those files
+for the first time.
+
+Signals an error if there is no current project."
+  (interactive (list (project-current)))
+  (unless proj
+    (user-error "There doesn't seem to be a project here"))
+  ;; Load the variables; they are stored buffer-locally, so...
+  (hack-dir-local-variables)
+  ;; Hold onto them...
+  (let ((locals dir-local-variables-alist))
+    (dolist (buffer (buffer-list))
+      (with-current-buffer buffer
+        (when (and (equal proj (project-current))
+                   buffer-file-name)
+          ;; transfer the loaded values to this buffer...
+          (setq-local dir-local-variables-alist locals)
+          ;; and apply them.
+          (hack-local-variables-apply))))))
+
 ;; General Helpers ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun af/read-file (path)
