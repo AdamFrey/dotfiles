72b53f2e4173fdccf35d91843a6309ed1205caef
Adam Frey <adam@adamfrey.me>
2025-03-21 16:04:37 -0400

    Remove broken package

--- nix/sources/doom/packages.el
+++ nix/sources/doom/packages.el
@@ -8,7 +8,7 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 
-(package! aidermacs :recipe (:host github :repo "MatthewZMD/aidermacs" :files ("*.el")))
+;;(package! aidermacs :recipe (:host github :repo "MatthewZMD/aidermacs" :files ("*.el")))
 (package! aggressive-indent)
 (package! sql-indent)
 (package! elisp-benchmarks)
