ad7627648c310f1b4a6be5fcfa732bbf4121c80e
Adam Frey <adam@adamfrey.me>
2025-03-28 08:41:06 -0400

    Pull profiles.clj into nix dotfiles

--- nix/sources/lein/profiles.clj
+++ nix/sources/lein/profiles.clj
@@ -1,11 +1,12 @@
 {:user {:dependencies
         [[djblue/portal "0.51.1"]
          [io.github.paintparty/fireworks "0.10.4"]
+         [io.github.tonsky/clojure-plus "1.3.1"]
          [org.clojars.abhinav/snitch "0.1.15"
           :exclusions [org.clojure/clojurescript]]
          [com.clojure-goes-fast/clj-java-decompiler "0.3.3"]
          [cider/cider-nrepl "0.45.0"]
          [refactor-nrepl/refactor-nrepl "3.9.1"]
          [org.clojure/tools.namespace "1.4.4"]]
-        :resource-paths ["/home/adam/.config/clojure/my.dev-tools/src"]
+        :resource-paths ["/home/adam/src/dotfiles/nix/sources/clojure/my.dev-tools"]
         :plugins        [[cddr/lein-pprint "1.3.2"]]}}
