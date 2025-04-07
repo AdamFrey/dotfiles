7fab33cd8b0dc5a8e66cb9f59ead552b61913dd8
Adam Frey <adam@adamfrey.me>
2025-04-07 13:44:07 -0400

    Bump clojure tool snitch version

--- nix/sources/lein/profiles.clj
+++ nix/sources/lein/profiles.clj
@@ -2,11 +2,11 @@
         [[djblue/portal "0.51.1"]
          [io.github.paintparty/fireworks "0.10.4"]
          [io.github.tonsky/clojure-plus "1.3.1"]
-         [org.clojars.abhinav/snitch "0.1.15"
+         [org.clojars.abhinav/snitch "0.1.16"
           :exclusions [org.clojure/clojurescript]]
          [com.clojure-goes-fast/clj-java-decompiler "0.3.3"]
          [cider/cider-nrepl "0.45.0"]
          [refactor-nrepl/refactor-nrepl "3.9.1"]
          [org.clojure/tools.namespace "1.4.4"]]
-        :resource-paths ["/home/adam/src/dotfiles/nix/sources/clojure/my.dev-tools"]
+        :resource-paths ["/home/adam/src/dotfiles/nix/sources/clojure/my.dev-tools/src"]
         :plugins        [[cddr/lein-pprint "1.3.2"]]}}
