25dbf92396b0c529a6e42cc4e202666153a83d92
Adam Frey <adam@adamfrey.me>
2025-05-19 07:57:43 -0400

    Suspend to deep sleep

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -21,6 +21,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.kernelParams = [ "mem_sleep_default=deep" ];
+
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
