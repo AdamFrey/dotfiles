5ab7e4197867e8cbd438e4f84281d6d5c0db7947
Adam Frey <adam@adamfrey.me>
2025-06-28 08:46:53 -0400

    turmn off wifi on laptop suspend

--- nix/framework-laptop.nix
+++ nix/framework-laptop.nix
@@ -1,3 +1,12 @@
+{ pkgs, ... }:
 {
   networking.hostName = "framework-laptop-nixos";
+
+  powerManagement.powerDownCommands = ''
+  ${pkgs.kmod}/bin/modprobe -r mt7925e || true
+'';
+
+  powerManagement.resumeCommands = ''
+  ${pkgs.kmod}/bin/modprobe mt7925e || true
+'';
 }
