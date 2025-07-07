40f2e6fe8a4a78232b9a78f3d000be822cf47dec
Adam Frey <adam@adamfrey.me>
2025-07-07 14:15:43 -0400

    Try installing zoom package

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -170,6 +170,7 @@
     zeal
     zenith
     zip
+    zoom-us
     zoxide
     inputs.claude-desktop.packages.${system}.claude-desktop-with-fhs
     inputs.zen-browser.packages.${pkgs.system}.default
