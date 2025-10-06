58dd3d026c62e66a6df7a35e49a72677fc7fc6b8
Adam Frey <adam@adamfrey.me>
2025-10-05 22:01:24 -0400

    Put back in wifi powermanagement commands

--- nix/framework-laptop.nix
+++ nix/framework-laptop.nix
@@ -2,11 +2,11 @@
 {
   networking.hostName = "framework-laptop-nixos";
 
- # powerManagement.powerDownCommands = ''
-#  ${pkgs.kmod}/bin/modprobe -r mt7925e || true
-#'';
+  powerManagement.powerDownCommands = ''
+  ${pkgs.kmod}/bin/modprobe -r mt7925e || true
+'';
 
-#  powerManagement.resumeCommands = ''
-#  ${pkgs.kmod}/bin/modprobe mt7925e || true
-#'';
+  powerManagement.resumeCommands = ''
+  ${pkgs.kmod}/bin/modprobe mt7925e || true
+'';
 }
