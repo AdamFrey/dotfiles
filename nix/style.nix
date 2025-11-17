9d643549bb1a90a32ec7ae0b24ba5f148d28725f
Adam Frey <adam@adamfrey.me>
2025-11-17 13:30:19 -0500

    Update the stylix theme

--- nix/style.nix
+++ nix/style.nix
@@ -1,20 +1,15 @@
 { config, pkgs, lib, ... }:
 {
     stylix = {
-        base16Scheme = "${pkgs.base16-schemes}/share/themes/selenized-light.yaml";
+        base16Scheme = "${pkgs.base16-schemes}/share/themes/selenized-dark.yaml";
         image = config.lib.stylix.pixel "base0A";
-        polarity = "light";
-
-        # image = "~/Downloads/aerial-forest-wallpaper.jpg";
-        # image = pkgs.fetchurl {
-        #   url = "https://images.unsplash.com/photo-1542919242-b32f2a7bce8d?ixlib=rb-4.0.3&q=85&fm=jpg&crop=entropy&cs=srgb";
-        #   sha256 = "foobar";
-        # };
+        polarity = "dark";
         enable = true;
     };
 
     specialisation.day.configuration = {
       stylix = {
+        base16Scheme = lib.mkForce "${pkgs.base16-schemes}/share/themes/selenized-light.yaml";
         polarity = lib.mkForce "light";
       };
     };
