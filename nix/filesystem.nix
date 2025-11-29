d1100f661e5375ca0927fb830da3abd0a21198c8
Adam Frey <adam@adamfrey.me>
2025-11-29 09:44:23 -0500

    Try adding new nfs options

--- nix/filesystem.nix
+++ nix/filesystem.nix
@@ -3,7 +3,17 @@ let
   nasMount = path: {
     device = "192.168.0.221:/mnt/pithos/${path}";
     fsType = "nfs";
-    options = [ "x-systemd.automount" "x-systemd.requires=network-online.target" "x-systemd.device-timeout=500ms" ];
+    options = [
+      "x-systemd.automount"
+      "x-systemd.requires=network-online.target"
+      "x-systemd.device-timeout=500ms"
+      "x-systemd.mount-timeout=2" # Fail mount attempts after 2 seconds
+      "x-systemd.idle-timeout=600" # disconnects after 10 minutes of inactivity
+      "noauto"
+      "soft"      # Return errors instead of hanging
+      "timeo=10"  # NFS timeout in deciseconds (1 second)
+      "retrans=1" # Only retry once before failing
+    ];
   };
   root = "/home/adam/nas";
 in
