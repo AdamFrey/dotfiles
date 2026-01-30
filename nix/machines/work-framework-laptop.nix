5b64d9c3140688b280896de09b0e1d2c8541f4e5
Adam Frey <adam@adamfrey.me>
2026-01-30 08:31:44 -0500

    Install lens kubernetes package

--- nix/machines/work-framework-laptop.nix
+++ nix/machines/work-framework-laptop.nix
@@ -2,6 +2,10 @@
 {
   networking.hostName = "work-laptop-nixos";
 
+  environment.systemPackages = with pkgs; [
+    freelens-bin
+  ];
+
   powerManagement.powerDownCommands = ''
   ${pkgs.kmod}/bin/modprobe -r mt7925e || true
 '';
